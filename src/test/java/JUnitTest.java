d569418de5862f018d6d7ecc3a0d76f3e1f75245
maja <maya-kut@mail.ru>
2025-04-14 20:10:33 +0300

    added gradle

---
+++ src/test/java/JUnitTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class JUnitTest {
+
+    @BeforeEach
+    void setUp() {}
+
+@Test
+void firstTest() {}
+}
