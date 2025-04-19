b6335477806904694bb2392394610913f47dbe61
maja <maya-kut@mail.ru>
2025-04-19 23:47:37 +0300

    added CssXpathExamples

--- src/test/java/TextBoxTest.java
+++ src/test/java/TextBoxTest.java
@@ -8,8 +8,7 @@ import java.io.File;
 import static com.codeborne.selenide.Condition.image;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TextBoxTest {
     private File Users;
@@ -17,7 +16,7 @@ public class TextBoxTest {
     @BeforeAll
 static void beforeAll() {
     Configuration.browserSize = "1920x1080";
-    Configuration.browser = "chrome";
+//    Configuration.browser = "chrome";
     Configuration.baseUrl = "https://demoqa.com";
     Configuration.pageLoadStrategy = "eager";
     Configuration.timeout = 3000;
@@ -27,31 +26,31 @@ static void beforeAll() {
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
-        $("[id=firstName]").setValue("Maiya").pressEnter();
-        $("[id=lastName]").setValue("M").pressEnter();
-        $("[id=userEmail]").setValue("maiya@mail.ru").pressEnter();
-        $("[id=genterWrapper]").$(byText("Female")).click();
-        $("[id=userNumber]").setValue("88009008890").pressEnter();
+        $("#firstName").setValue("Maiya").pressEnter();
+        $("#lastName").setValue("M").pressEnter();
+        $("#userEmail").setValue("maiya@mail.ru").pressEnter();
+        $("#genterWrapper").$(byText("Female")).click();
+        $("#userNumber").setValue("88009008890").pressEnter();
 
-        $("#dateOfBirthInput").click();
+        $("#dateOfBirthInput").pressEnter();
 
-        $("[class=react-datepicker__month-select]").selectOption("June");
-        $("[class=react-datepicker__year-select]").selectOption("1996");
-        $("[class=react-datepicker__day]").$(byText("25")).click();
+        $(".react-datepicker__month-select").selectOption("June");
+        $(".react-datepicker__year-select").selectOption("1996");
+        $$("div.react-datepicker__day").findBy(text("25")).pressEnter();
 
-        $("[id=subjectsInput]").setValue("M").pressEnter();
-        $("[id=subjectsInput]").setValue("G").pressEnter();
+        $("#subjectsInput").setValue("M").pressEnter();
+        $("#subjectsInput").setValue("B").pressEnter();
 
-        $("[id=hobbiesWrapper]").find("Sports").click();
-        $("[id=hobbiesWrapper]").find("Reading").click();
-        $("[id=hobbiesWrapper]").find("Music").click();
+        $("label[for=hobbies-checkbox-1]").click();
+        $("label[for=hobbies-checkbox-2]").click();
+        $("label[for=hobbies-checkbox-3]").click();
 
-        $("[id=uploadPicture]").uploadFromClasspath(image.png);
+//        $("#uploadPicture").uploadFromClasspath(image.png);
 
-        $("[id=uploadPicture]").setValue("Moscow, Red Square");
+        $("#uploadPicture").setValue("Moscow, Red Square");
 
-        $("[id=react-select-3-input]").setValue("Haryana").pressEnter();
-        $("[id=react-select-4-input]").setValue("Panipat").pressEnter();
+        $("#react-select-3-input").setValue("Haryana").pressEnter();
+        $("#react-select-4-input").setValue("Panipat").pressEnter();
 
         $("[id=submit]").click();
 
