9db70942c38649c93ca90998a6a4a9c41a883b16
maja <maya-kut@mail.ru>
2025-04-25 11:58:18 +0300

    perfect code

--- src/test/java/TextBoxTest.java
+++ src/test/java/TextBoxTest.java
@@ -1,5 +1,8 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -11,36 +14,40 @@ public class TextBoxTest {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
-//    Configuration.browser = "chrome";
+        Configuration.browser = "chrome";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
     }
 
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
-        $("#firstName").setValue("Maiya").pressEnter();
-        $("#lastName").setValue("M").pressEnter();
-        $("#userEmail").setValue("maiya@mail.ru").pressEnter();
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        $("#firstName").setValue("Maiya");
+        $("#lastName").setValue("M");
+        $("#userEmail").setValue("maiya@mail.ru");
         $("#genterWrapper").$(byText("Female")).click();
-        $("#userNumber").setValue("88009008890").pressEnter();
+        $("#userNumber").setValue("8800900889");
 
-        $("#dateOfBirthInput").pressEnter();
+        $("#dateOfBirthInput").click();
 
         $(".react-datepicker__month-select").selectOption("June");
         $(".react-datepicker__year-select").selectOption("1996");
-        $$("div.react-datepicker__day").findBy(text("25")).pressEnter();
+        $$("div.react-datepicker__day").findBy(text("25")).click();
 
         $("#subjectsInput").setValue("M").pressEnter();
         $("#subjectsInput").setValue("B").pressEnter();
 
-        $("label[for=hobbies-checkbox-1]").click();
-        $("label[for=hobbies-checkbox-2]").click();
-        $("label[for=hobbies-checkbox-3]").click();
+//        $("label[for='hobbies-checkbox-1']").click();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+//        $("label[for=hobbies-checkbox-2]").click();
+//        $("label[for=hobbies-checkbox-3]").click();
 
         $("#uploadPicture").uploadFromClasspath("image.png");
-
-        $("#uploadPicture").setValue("Moscow, Red Square");
+//
+        $("#currentAddress").setValue("Moscow, Red Square");
         $("#state").click();
         $("#react-select-3-input").setValue("Haryana").pressEnter();
         $("#city").click();
@@ -52,10 +59,10 @@ public class TextBoxTest {
         $$(".table-responsive").findBy(text("Student Name")).shouldHave(text("Maiya M"));
         $$(".table-responsive").findBy(text("Student Email")).shouldHave(text("maiya@mail.ru"));
         $$(".table-responsive").findBy(text("Gender")).shouldHave(text("Female"));
-        $$(".table-responsive").findBy(text("Mobile")).shouldHave(text("88009008890"));
+        $$(".table-responsive").findBy(text("Mobile")).parent().shouldHave(text("8800900889"));
         $$(".table-responsive").findBy(text("Date of Birth")).shouldHave(text("25 June,1996"));
         $$(".table-responsive").findBy(text("Subjects")).shouldHave(text("Maths, Biology"));
-        $$(".table-responsive").findBy(text("Hobbies")).shouldHave(text("Sports, Reading, Music"));
+        $$(".table-responsive").findBy(text("Hobbies")).shouldHave(text("Sports"));
         $$(".table-responsive").findBy(text("Picture")).shouldHave(text("image.png"));
         $$(".table-responsive").findBy(text("Address")).shouldHave(text("Moscow, Red Square"));
         $$(".table-responsive").findBy(text("State and City")).shouldHave(text("Haryana Panipat"));
