e9c17ad10be71090c50fcffab5e2500af83609f6
maja <maya-kut@mail.ru>
2025-05-04 22:33:54 +0300

    add structure

--- src/test/java/tests/ClickSubmitWithoutFillingFields.java
+++ src/test/java/tests/ClickSubmitWithoutFillingFields.java
@@ -1,10 +1,10 @@
+package tests;
+
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class ClickSubmitWithoutFillingFields {
