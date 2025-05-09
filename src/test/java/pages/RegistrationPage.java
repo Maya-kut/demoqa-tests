0cb2b4f1b99b36f80c9103ebd2e66d42b396a000
maja <maya-kut@mail.ru>
2025-05-09 19:15:10 +0300

    POM add

---
+++ src/test/java/pages/RegistrationPage.java
@@ -0,0 +1,124 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationPage {
+    private SelenideElement firstNameInput = $("#firstName");
+    private SelenideElement lastNameInput = $("#lastName");
+    private SelenideElement userEmailInput = $("#userEmail");
+    private SelenideElement genderWrapper = $("#genterWrapper");
+    private SelenideElement userNumberInput = $("#userNumber");
+    private SelenideElement calendarInput = $("#dateOfBirthInput");
+    private SelenideElement subjectInput = $("#subjectsInput");
+    private SelenideElement hobbiesCheckbox = $("label[for=hobbies-checkbox-2]");
+    private SelenideElement uploadPicture = $("#uploadPicture");
+    private SelenideElement AddressTextArea = $("#currentAddress");
+    private SelenideElement selectState = $("#state");
+    private SelenideElement getSelectState = $("#react-select-3-input");
+    private SelenideElement selectCity = $("#city");
+    private SelenideElement getSelectCity = $("#react-select-4-input");
+    private SelenideElement submitButton = $("#submit");
+    private SelenideElement closeButton = $("#closeLargeModal");
+
+    CalendarComponent calendarComponent = new CalendarComponent();
+
+    public RegistrationPage openPage() {
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        return this;
+    }
+
+    public RegistrationPage setFirstName(String value) {
+        firstNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setLastName(String value) {
+        lastNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setEmail(String value) {
+        userEmailInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setGender(String value) {
+        genderWrapper.$(byText(value)).click();
+
+        return this;
+    }
+
+    public RegistrationPage setUserNumber(String value) {
+        userNumberInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setDateOfBirth(String day, String month, String year) {
+        calendarInput.click();
+        calendarComponent.setDate(day, month, year);
+
+        return this;
+    }
+
+    public RegistrationPage setSubjects(String value, String subjectTwo) {
+        subjectInput.setValue(value).pressEnter();
+
+        return this;
+    }
+
+    public RegistrationPage setHobbies(String hobbie) {
+        hobbiesCheckbox.click();
+
+        return this;
+    }
+
+    public RegistrationPage upLoadPicture() {
+        uploadPicture.uploadFromClasspath("image.png");
+
+        return this;
+    }
+
+    public RegistrationPage setAddress(String address) {
+        AddressTextArea.setValue(address);
+
+        return this;
+    }
+
+    public RegistrationPage setState(String state) {
+        selectState.click();
+        getSelectState.find(String.valueOf(text(state))).click();
+        return this;
+    }
+
+    public RegistrationPage setCity(String city) {
+        selectCity.click();
+        getSelectCity.find(String.valueOf(text(city))).click();
+        return this;
+    }
+
+    public void clickSubmitButton() {
+        submitButton.click();
+    }
+
+    public RegistrationPage checkResult(String key, String value) {
+        $(".table-responsive").$(byText(key)).parent()
+                .shouldHave(text(value));
+
+        return this;
+    }
+    public void clickCloseLargeModal(){
+        closeButton.click();
+    }
+}
+
---
+++ src/test/java/tests/PracticeFormPOM.java
@@ -0,0 +1,57 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
+
+public class PracticeFormPOM extends TestBase {
+    String firstName = "Maiya";
+    String lastName = "M";
+    String email = "maiya@mail.ru";
+    String gender = "Female";
+    String mobile = "8800900889";
+    String birthDay = "25";
+    String birthMonth = "June";
+    String birthYear = "1996";
+    String subjectOne = "Biology";
+    String subjectTwo = "Math";
+    String hobbie = "Sports";
+    String address = "Moscow, Red Square";
+    String state = "Haryana";
+    String city = "Panipat";
+
+    RegistrationPage RegistrationPage = new RegistrationPage();
+
+    @Test
+//Отправка формы со всеми заполненными полями
+    void PracticeFormTestAllFields() {
+        RegistrationPage.openPage()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setGender(gender)
+                .setUserNumber(mobile)
+                .setDateOfBirth(birthDay, birthMonth, birthYear)
+                .setSubjects(subjectOne, subjectTwo)
+                .setHobbies(hobbie)
+                .upLoadPicture()
+                .setAddress(address)
+                .setState(state)
+                .setCity(city)
+                .clickSubmitButton();
+
+        //Проверка корректности заполнения формы
+        RegistrationPage.checkResult("Student Name", "Maiya M")
+                .checkResult("Student Email", "maiya@mail.ru")
+                .checkResult("Gender", "Female")
+                .checkResult("Mobile", "8800900889")
+                .checkResult("Date of Birth", "25 June,1996")
+                .checkResult("Subjects", "Biology, Maths")
+                .checkResult("Hobbies", "Sports")
+                .checkResult("Picture", "image.png")
+                .checkResult("Address", "Moscow, Red Square")
+                .checkResult("State and City", "Haryana Panipat")
+                .clickCloseLargeModal();
+
+
+    }
+}
