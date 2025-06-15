d2be5a8e15d121b73e7e66a227d8ec8c29e92ade
maja <maya-kut@mail.ru>
2025-06-15 17:16:46 +0300

    add property

---
+++ src/test/java/properties/SystemPropertiesTests.java
@@ -0,0 +1,19 @@
+package properties;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class SystemPropertiesTests {
+    @Test
+    @Tag("property")
+    void systemProperties4Test() {
+        String browser = System.getProperty("browser", "mozilla");
+
+        System.out.println(browser);
+        // gradle property_test
+        // mozilla
+
+        // gradle property_test -Dbrowser=opera
+        // opera
+    }
+}
